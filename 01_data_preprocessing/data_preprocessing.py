d24f77df12d0c0657690cb3d309e1725410b2a88
Jordan Hudgens <jordan@wow.com>
2017-07-13 14:22:05 -0700

    Updated the raw data to fit better with preprocessor

--- 01_data_preprocessing/data_preprocessing.py
+++ 01_data_preprocessing/data_preprocessing.py
@@ -12,3 +12,9 @@ import pandas as pd # Data set management tool
 raw_data = pd.read_csv('raw_data.csv')
 features = raw_data.iloc[:, :-1].values
 dependent_variable = raw_data.iloc[:, 4].values
+
+# Fixing missing data
+from sklearn.preprocessing import Imputer
+imputer = Imputer(missing_values = "NaN", strategy = 'mean', axis = 0)
+imputer = imputer.fit(features[:, 0:2])
+features[:, 0:2] = imputer.transform(features[:, 0:2])
