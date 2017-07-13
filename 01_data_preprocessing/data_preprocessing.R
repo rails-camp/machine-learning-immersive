89382a32ba804dc0fc80716b58b014feb109efb5
Jordan Hudgens <jordan@wow.com>
2017-07-13 14:40:10 -0700

    Added R script for cleaning up missing columns

--- 01_data_preprocessing/data_preprocessing.R
+++ 01_data_preprocessing/data_preprocessing.R
@@ -1,1 +1,6 @@
+# Import the CSV data
 raw_data = read.csv('raw_data.csv')
+# Fix missing data
+raw_data$Age = ifelse(is.na(raw_data$Age),
+                      ave(raw_data$Age, FUN = function(x) mean(x, na.rm = TRUE)),
+                      raw_data$Age)
