a89306e76e5a3f23f45350db799ef01230f87bcf
Jordan Hudgens <jordan@wow.com>
2017-07-13 14:41:03 -0700

    Updated the data pre processor in R

--- 01_data_preprocessing/data_preprocessing.R
+++ 01_data_preprocessing/data_preprocessing.R
@@ -4,3 +4,6 @@ raw_data = read.csv('raw_data.csv')
 raw_data$Age = ifelse(is.na(raw_data$Age),
                       ave(raw_data$Age, FUN = function(x) mean(x, na.rm = TRUE)),
                       raw_data$Age)
+raw_data$Income = ifelse(is.na(raw_data$Income),
+                      ave(raw_data$Income, FUN = function(x) mean(x, na.rm = TRUE)),
+                      raw_data$Income)
